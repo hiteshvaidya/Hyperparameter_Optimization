2b9ae769e40e32ccccac9fea440d6ffbc08ef97e
Hitesh Vaidya <hiteshv.tech@gmail.com>
2021-04-27 17:50:47 -0400

    Initial commit

---
+++ src/skopt_optimization.py
@@ -0,0 +1,71 @@
+"""
+Hyperparameter tuning using Skopt
+"""
+
+import pandas as pd
+import numpy as np
+from sklearn import ensemble
+from sklearn import metrics
+from sklearn import model_selection
+from functools import partial
+from skopt import space
+from skopt import gp_minimize
+
+def optimize(params, param_names, x, y):
+    params = dict(zip(param_names, params))
+    model = ensemble.RandomForestClassifier(**params)
+    kf = model_selection.StratifiedKFold(n_splits=5)
+    accuracies = []
+    for idx in kf.split(X=x, y=y):
+        train_idx, test_idx = idx[0], idx[1]
+        xtrain = x[train_idx]
+        ytrain = y[train_idx]
+
+        xtest = x[test_idx]
+        ytest = y[test_idx]
+
+        model.fit(xtrain, ytrain)
+        preds = model.predict(xtest)
+        fold_acc = metrics.accuracy_score(ytest, preds)
+        accuracies.append(fold_acc)
+
+    return -1.0 * np.mean(accuracies) #since we want to minimize accuracy, therefore, multiply by -1
+
+
+def skopt_bayesian_optimization(X, y):
+
+    param_space = [
+        space.Integer(3, 15, name='max_depth'),
+        space.Integer(100, 600, name='n_estimators'),
+        space.Categorical(['gini', 'entropy'], name='criterion'),
+        space.Real(0.01, 1, prior='uniform', name='max_features')
+    ]
+
+    param_names = [
+        'max_depth',
+        'n_estimators',
+        'criterion',
+        'max_features'
+    ]
+
+    optimization_function = partial(
+        optimize,
+        param_names=param_names,
+        x=X,
+        y=y
+    )
+
+    result = gp_minimize(
+        optimization_function,
+        dimensions=param_space,
+        n_calls=15,
+        n_random_starts=10,
+        verbose=10
+    )
+    print(dict(zip(param_names, result.x)))
+
+if __name__ == '__main__':
+    df = pd.read_csv('/home/group2/research/nac/data/hitesh/Hyperparameter Optimization/data/train.csv')
+    X = df.drop('price_range', axis=1).values
+    y = df.price_range.values
+    skopt_bayesian_optimization(X, y)
