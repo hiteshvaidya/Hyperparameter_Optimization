b9d5756b1b8bc4159a7ed96929218afe1e3d0d51
Hitesh Vaidya <hiteshv.tech@gmail.com>
2021-04-28 19:47:53 -0400

    update train and utils

--- Mechanism of Action/src/utils.py
+++ Mechanism of Action/src/utils.py
@@ -1,4 +1,5 @@
 import torch
+import torch.nn as nn
 
 class MoaDataset:
     def __init__(self, features, targets):
@@ -13,4 +14,65 @@ class MoaDataset:
             'x': torch.tensor(self.features[item, :], dtype=torch.float),
             'y': torch.tensor(self.targets[item, :], dtype=torch.float)
         }
-    
+
+
+class Engine:
+
+    def __init__(self, model, optimizer, device):
+        self.model = model
+        self.device = device
+        self.optimizer = optimizer
+
+    @staticmethod
+    def loss_fn(targets, outputs):
+        loss = nn.BCEWithLogitsLoss(outputs, targets)
+        return loss
+
+    def train(self, data_loader):
+        self.model.train()
+        final_loss = 0
+        for data in data_loader:
+            self.optimizer.zero_grad()
+            inputs = data['x'].to(self.device)
+            targets = data['y'].to(self.device)
+            outputs = self.model(inputs)
+            loss = self.loss_fn(targets, outputs)
+            loss.backward()
+            self.optimizer.step()
+            final_loss += loss.item()
+        return final_loss / len(data_loader)
+
+    def evaluate(self, data_loader):
+        self.model.eval()
+        final_loss = 0
+        for data in data_loader:
+            inputs = data['x'].to(self.device)
+            targets = data['y'].to(self.device)
+            outputs = self.model(inputs)
+            loss = self.loss_fn(targets, outputs)
+            final_loss += loss.item()
+        return final_loss / len(data_loader)
+
+
+class Model(nn.Module):
+
+    def __init__(self, n_features, n_targets, n_layers, hidden_size, dropout):
+        super().__init__()
+        layers = []
+        for _ in range(n_layers):
+            if len(layers) == 0:
+                layers.append(nn.Linear(n_features, hidden_size))
+                layers.append(nn.BatchNorm1d(hidden_size))
+                layers.append(nn.Dropout(dropout))
+                layers.append(nn.ReLU())
+            else:
+                layers.append(nn.Linear(hidden_size, hidden_size))
+                layers.append(nn.BatchNorm1d(hidden_size))
+                layers.append(nn.Dropout(dropout))
+                layers.append(nn.ReLU())
+        layers.append(nn.Linear(hidden_size, n_targets))
+        self.model = nn.Sequential(*layers)
+
+    def forward(self, x):
+        return self.model(x)
+
